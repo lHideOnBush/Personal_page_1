d0b424bf575c63fc3fbeee8cdb67f7cdf762174a
Frederick <fredsantiago006@gmail.com>
2022-03-27 11:43:37 +0800

    Login SubSystem v0.1

--- index.php
+++ index.php
@@ -14,85 +14,64 @@
         $num=10;
         for ($i=0; $i < $num ; $i++) { 
             # code...
-            ?> <div class="">
+            ?> 
+            <div class="">
                 <h2><?php echo $i ?></h2>
             </div>
             <?php
-        }   ?>
+        }   
+    ?>
 
-<div class="">
+    <div class="">
         <h1>---------------------------------------------
     </div>
  
         <div class="">
             <?php
-            $num=60;
-            for ($i=0; $i < $num ; $i+=10) { 
-                # code...
-                ?> <div class="">
-                    <h2><?php echo $i ?>frederick</h2>
-                </div>
-                <?php
-            }   ?>
-<div class="">
+                $num=60;
+                for ($i=0; $i < $num ; $i+=10) { 
+                    # code...
+                    ?> 
+                    <div class="">
+                        <h2><?php echo $i ?>frederick</h2>
+                    </div>
+                    <?php
+                }   
+            ?>
+    <div class="">
         <h1>---------------------------------------------
     </div>         
-         <?php
-         $input =5;
-         if ($input > 3){
-             ?>
-             <div class="">
-                 <h1>NotB</h1>
-             </div>
-             <?php
-         }
-         else{
-             ?>
-             <div class="">
+        <?php
+            $input =5;
+            if ($input > 3){
+            ?>
+            <div class="">
+                <h1>NotB</h1>
+            </div>
+            <?php
+        } else {
+            ?>
+            <div class="">
 
-             </div>  
-             <h1>NotA</h1>
-             </div>
-             <?php
-         }
-         
-         ?>
-         <div class="">
-        <h1>---------------------------------------------
-    </div>
+            </div>  
+            <h1>NotA</h1>
+            </div>
+            <?php
+            } 
+        ?>
+        <div class="">
+            <h1>---------------------------------------------
+        </div>
             
-
-
-    <div class="">
 <?php
-$input =11;
-if ($input > 5) {   # code...
-    ?> <div class="">
-                 <h1> (10,20,30,40,50)</h1>
-             </div>
-    <?php
-} 
-else if ($input < 5)
-{?>
-    <div class="">
-    </div>  
-    <h1>EVEN</h1>
-    </div>
-    <?php
-}
-
-else{
-    ?>
-    <div class="">
-    </div>  
-    <h1>ODD</h1>
-    </div>
-    <?php
-}
+    $total = 0;
+    for($i=0; $i<=10; $i++)
+    {
+        $sum = $total + $i;
+    }
+    $average = $sum / 5;
+    echo "Average : $average"."\n";
 ?>
-}
-
-    </div> 
 
 </body>
 </html>
